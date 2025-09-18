61883479f623655957c653f823e87e241df4afd2
Varsha Rathore <147488359+varsharrathore@users.noreply.github.com>
2025-09-18 20:14:59 +0530

    ScrollToTop.jsx

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -2,11 +2,18 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, [pathname]);
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    }
+  }, [pathname, hash]);
 
   return null;
 }
